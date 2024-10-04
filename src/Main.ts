ac08507c131c0bb2dff16a18552be7c5762d66b3
Aurailus <me@auri.xyz>
2024-10-04 14:57:34 -0700

    Project Framework

---
+++ src/Main.ts
@@ -0,0 +1,8 @@
+import { h, render } from 'preact';
+
+import App from './components/App';
+
+import './Style.pcss';
+
+render(h(App, {}), document.body);
+
---
+++ src/components/App.tsx
@@ -0,0 +1,5 @@
+import { Fragment, h } from 'preact';
+
+export default function App() {
+	return <Fragment>Hi</Fragment>
+}
